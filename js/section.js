dd0d8f393e48692cf22e64adbb6111c017cca8d1
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-06-13 18:32:42 +0530

    Added iframe for Youtube playlists

--- js/section.js
+++ js/section.js
@@ -16,15 +16,18 @@ t.render(function(){
     return t.get('card', 'private', 'ytURL').then(function(url){
       if (url.startsWith("https://www.youtube.com") || url.startsWith("https://m.youtube.com") || url.startsWith("http://www.youtube.com") || url.startsWith("http://m.youtube.com")){
         if (url.includes("watch")){
-          console.log("URL is a Video");
           yt_code = url.split("=")[1];
-        }       
+          var iframe = '<iframe width="853" height="480" src="https://www.youtube.com/embed/'+yt_code+'" frameborder="0" allowfullscreen></iframe>';
+        }else if (url.includes("playlist")){
+          yt_code = url.split("=")[1];
+          var iframe = '<iframe width="720" height="405" src="https://www.youtube.com/embed/?listType=playlist&list='+yt_code+'" frameborder="0" allowfullscreen>'
+        }
         
       }else if (url.startsWith("https://youtu.be") || url.startsWith("http://youtu.be")){
         yt_code = url.split("/")[3];
+        var iframe = '<iframe width="853" height="480" src="https://www.youtube.com/embed/'+yt_code+'" frameborder="0" allowfullscreen></iframe>';
       }
       // ytURL = JSON.stringify(url, null, 2);
-      var iframe = '<iframe width="853" height="480" src="https://www.youtube.com/embed/'+yt_code+'" frameborder="0" allowfullscreen></iframe>';
       document.getElementById('video').innerHTML = iframe;    
     })
   })
