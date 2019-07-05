1f8b3196c8fca299e6b4f22e406813e63edd0621
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 07:51:58 +0530

    test line

--- js/settingsSocialLinks.js
+++ js/settingsSocialLinks.js
@@ -6,6 +6,7 @@ var t = TrelloPowerUp.iframe();
 document.getElementById('embedFeedBtn').addEventListener('click', function(){
   var context = t.getContext();
   $.get("https://us-central1-fiverr-oikasocial.cloudfunctions.net/member?id="+context.member+"&boardID="+context.board, function(resp){
+    console.log("ccc");
     console.log(resp);
   })
 })
