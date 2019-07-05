b0144c4d4088246cf4b82217e6d6be65545e5e84
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 09:22:54 +0530

    added test line to attach urls

--- js/client.js
+++ js/client.js
@@ -293,6 +293,7 @@ TrelloPowerUp.initialize({
       // potentially length operation you can provide a function for the title
       // that returns the section title. If you do so, provide a unique id for
       // your section
+      console.log(attachment.url);
       if (attachment.url == "https://www.facebook.com/helarapwadan/"){
         return [
       {
