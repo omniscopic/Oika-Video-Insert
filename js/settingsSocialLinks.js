185605c161cdd4170d9c9ad60c43a9e6336161ad
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 08:54:54 +0530

    url

--- js/settingsSocialLinks.js
+++ js/settingsSocialLinks.js
@@ -4,7 +4,7 @@ var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
 document.getElementById('embedFeedBtn').addEventListener('click', function(){
-  return t.set('card', 'shared', 'socialUrl', "SOCIAL FEED").then(function(){
+  return t.set('card', 'shared', 'socialUrl', "https://www.facebook.com/helarapwadan/").then(function(){
     t.attach({url: "https://www.facebook.com/helarapwadan/"});
     t.closePopup();
   })
