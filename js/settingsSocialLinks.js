7e801eca9b075f1e70a4863cfbe9f4c564397e63
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 07:45:36 +0530

    added get request to get data from firebase

--- js/settingsSocialLinks.js
+++ js/settingsSocialLinks.js
@@ -4,7 +4,8 @@ var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
 document.getElementById('embedFeedBtn').addEventListener('click', function(){
-  console.log("Clicked");
   var context = t.getContext();
-  console.log(JSON.stringify(context, null, 2));
+  $.get("https://us-central1-fiverr-oikasocial.cloudfunctions.net/member?id="+context.member+"&boardID="+context.board, function(resp){
+    console.log(resp);
+  })
 })
