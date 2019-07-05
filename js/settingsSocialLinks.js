a274e41b1ef031bf780196bbdb6215bbf38dda49
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 08:28:26 +0530

    t.set

--- js/settingsSocialLinks.js
+++ js/settingsSocialLinks.js
@@ -4,6 +4,9 @@ var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
 document.getElementById('embedFeedBtn').addEventListener('click', function(){
-  t.attach({socialUrl: "FB FEED"});
+  return t.set('card', 'shared', 'socialUrl', "SOCIAL FEED").then(function(){
+    t.attach({url: "SOCIAL FEED"});
+    t.closePopup();
+  })
   t.closePopup();
 });
