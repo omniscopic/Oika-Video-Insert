0b8f3da78799cd85af425a7d9e4677c9ed7d4027
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 08:52:58 +0530

    iframe added

--- js/sectionSocialLinks.js
+++ js/sectionSocialLinks.js
@@ -14,10 +14,12 @@ t.render(function(){
   })
   .then(function(socialUrl){
     var context = t.getContext();
-    $.get("https://us-central1-fiverr-oikasocial.cloudfunctions.net/member?id="+context.member+"&boardID="+context.board, function(resp){
+    /*$.get("https://us-central1-fiverr-oikasocial.cloudfunctions.net/member?id="+context.member+"&boardID="+context.board, function(resp){
       console.log(resp);
-    })
-    iframe = "<p> hello</p>"
+    })*/
+    url = encodeURIComponent(socialUrl)
+
+    iframe = '<iframe src="https://www.facebook.com/plugins/page.php?href='+url+'&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=2152623648118362" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>'
     document.getElementById('socialFeed').innerHTML = iframe;
   })
   .then(function(){
