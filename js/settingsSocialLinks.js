79f2d18baa1e9d7b1f8fca3c8d609622c9a06728
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-05 08:09:30 +0530

    removed get request

--- js/settingsSocialLinks.js
+++ js/settingsSocialLinks.js
@@ -5,8 +5,4 @@ var t = TrelloPowerUp.iframe();
 
 document.getElementById('embedFeedBtn').addEventListener('click', function(){
   var context = t.getContext();
-  $.get("https://us-central1-fiverr-oikasocial.cloudfunctions.net/member?id="+context.member+"&boardID="+context.board, function(resp){
-    console.log("ccc");
-    console.log(resp);
-  })
-})
+});
