7be935141fb20d15705d802147e7539f1b93bc51
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-06 19:49:44 +0530

    Added Window location assign

--- js/sectionSocialLinks.js
+++ js/sectionSocialLinks.js
@@ -9,7 +9,7 @@ t.render(function(){
   // from your section
   url = encodeURIComponent(t.arg('fbUrl'));
   //iframe = '<iframe src="https://www.facebook.com/plugins/page.php?href='+url+'&tabs=timeline&width=843&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=2152623648118362" width="843" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>'
-  iframe = '<script type="text/javascript"> window.location.href='+url+' </script>'
-  document.getElementById('socialFeed').innerHTML = iframe;
+  
+  document.getElementById('socialFeed').innerHTML = window.location.assign(t.arg('fbUrl'));
   return t.sizeTo(500);
 });
