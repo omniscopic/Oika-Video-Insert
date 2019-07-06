0ef407f1ac64fde280e1c92c7be57c47200deab0
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-06 07:18:02 +0530

    removed test lines

--- js/client.js
+++ js/client.js
@@ -278,9 +278,6 @@ TrelloPowerUp.initialize({
 
     // we will just claim urls for Yellowstone
     var claimed = options.entries.filter(function(attachment){
-      console.log(options);
-      console.log(attachment);
-      console.log(attachment.url);
       return attachment.url.indexOf('https://www.youtube.com/') === 0 || attachment.url.indexOf('https://m.youtube.com/') === 0 || attachment.url.indexOf('https://youtu.be/') === 0 || attachment.url.indexOf('https://www.facebook.com') === 0;
     });
 
@@ -296,7 +293,6 @@ TrelloPowerUp.initialize({
       // potentially length operation you can provide a function for the title
       // that returns the section title. If you do so, provide a unique id for
       // your section
-      console.log(attachment.url);
       if (attachment.url == "https://www.facebook.com/helarapwadan/"){
         return [
       {
