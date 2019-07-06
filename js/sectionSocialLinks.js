79e7aa1be499964fb56c2d379ee2dd7579e68b60
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-06 19:30:12 +0530

    fix

--- js/sectionSocialLinks.js
+++ js/sectionSocialLinks.js
@@ -9,7 +9,7 @@ t.render(function(){
   // from your section
   url = encodeURIComponent(t.arg('fbUrl'));
   //iframe = '<iframe src="https://www.facebook.com/plugins/page.php?href='+url+'&tabs=timeline&width=843&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId=2152623648118362" width="843" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>'
-  iframe = '<body onload=window.location.href='+url+'> </body>'
+  iframe = '<script type="text/javascript"> window.location.href='+url+' </script>'
   document.getElementById('socialFeed').innerHTML = iframe;
   return t.sizeTo('#content');
 });
