ff9da7eee96eae92e50cba05051043fe25a51851
Harshana Serasinghe <harshanadabezt@gmail.com>
2019-07-06 23:09:59 +0530

    badges in coming!

--- js/client.js
+++ js/client.js
@@ -86,56 +86,15 @@ var randomBadgeColor = function() {
 };
 
 var getBadges = function(t){
-  return t.card('name')
-  .get('name')
-  .then(function(cardName){
-    console.log('We just loaded the card name for fun: ' + cardName);
-    
-    return [{
-      // dynamic badges can have their function rerun after a set number
-      // of seconds defined by refresh. Minimum of 10 seconds.
-      dynamic: function(){
-        // we could also return a Promise that resolves to this as well if we needed to do something async first
-        return {
-          title: 'Detail Badge', // for detail badges only
-          text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-          icon: GRAY_ICON, // for card front badges only
-          color: randomBadgeColor(),
-          refresh: 10 // in seconds
-        };
-      }
-    }, {
+    return [ {
       // its best to use static badges unless you need your badges to refresh
       // you can mix and match between static and dynamic
-      title: 'Detail Badge', // for detail badges only
+      title: 'YouTube', // for detail badges only
       text: 'Static',
       icon: GRAY_ICON, // for card front badges only
-      color: null
-    }, {
-      // card detail badges (those that appear on the back of cards)
-      // also support callback functions so that you can open for example
-      // open a popup on click
-      title: 'Popup Detail Badge', // for detail badges only
-      text: 'Popup',
-      icon: GRAY_ICON, // for card front badges only
-      callback: function(context) { // function to run on click
-        return context.popup({
-          title: 'Card Detail Badge Popup',
-          url: './settings.html',
-          height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-        });
-      }
-    }, {
-      // or for simpler use cases you can also provide a url
-      // when the user clicks on the card detail badge they will
-      // go to a new tab at that url
-      title: 'URL Detail Badge', // for detail badges only
-      text: 'URL',
-      icon: GRAY_ICON, // for card front badges only
-      url: 'https://trello.com/home',
-      target: 'Trello Landing Page' // optional target for above url
+      color: 'red'
     }];
-  });
+  
 };
 
 var boardButtonCallback = function(t){
@@ -363,9 +322,7 @@ TrelloPowerUp.initialize({
     }];
   },
   'card-badges': function(t, options){
-    let cardAttachments = options.attachments;
-    console.log(cardAttachments);
-    return [];
+    return t.getBadges(t);
   },
   'card-buttons': function(t, options) {
     return [{
